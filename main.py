74bbe7e5c0ac5753e79cb3fa6902c432976d4889
emialbasta <emialbaster2@hotmail.com>
2025-05-12 17:22:18 -0700

    bookbot updated to count words in document

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ def get_book_text(path_to_file):
 	with open(path_to_file) as f:
 		file_contents = f.read()
 	return file_contents
+
+def count_words(text):
+        words = text.split()
+        return len(words)
+
 def main():
-	print(get_book_text("books/frankenstein.txt"))
+	text = get_book_text("books/frankenstein.txt")
+	word_count = count_words(text)
+	print(f"{word_count} words found in the document")
 main()
