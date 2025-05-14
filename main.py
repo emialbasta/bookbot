5e139e33872afc0a0ca304e7b9ab68cd4bd37619
emialbasta <emialbaster2@hotmail.com>
2025-05-14 15:16:06 -0700

    added sys arguement for paths to books

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import sys
+
 def get_book_text(path_to_file):
 	with open(path_to_file) as f:
 		file_contents = f.read()
 	return file_contents
-
-path = "books/frankenstein.txt"
 
 from stats import count_words
 
@@ -12,12 +12,16 @@ from stats import count_characters
 from stats import sort_chars_by_count
 
 def main():
-	text = get_book_text("books/frankenstein.txt")
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	path_to_book = sys.argv[1]
+	text = get_book_text(path_to_book)
 	word_count = count_words(text)
 	characters = count_characters(text)
 	word_list = sort_chars_by_count(characters)
 	print ("============ BOOKBOT ============")
-	print (f"Analyzing book found at {path}")
+	print (f"Analyzing book found at {path_to_book}")
 	print ("----------- Word Count ----------")
 	print (f"Found {word_count} total words")
 	print ("--------- Character Count -------")
